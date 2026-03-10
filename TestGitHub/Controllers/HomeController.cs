e7e28cb5604d8d5f70570a68e5d6446663120686
koviedoa812 <koviedoa-m@ucenfotec.ac.cr>
2026-03-10 12:52:19 -0600

    Feature Contacto Creado

--- TestGitHub/Controllers/HomeController.cs
+++ TestGitHub/Controllers/HomeController.cs
@@ -23,6 +23,13 @@ namespace TestGitHub.Controllers
             return View();
         }
 
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
